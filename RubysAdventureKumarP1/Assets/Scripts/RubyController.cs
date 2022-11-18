e5c044e5bbf9396dd43d9b4a9cc22083d327dd0e
Ashtonval <tennisjedi20xx@gmail.com>
2022-11-18 09:53:47 -0800

    Changed the collider and created more code
    The  collider was adjusted so that the game is smoother.  Code for RubyController was fixed.  Code was created for health by adding a health game object.

--- RubysAdventureKumarP1/Assets/Scripts/RubyController.cs
+++ RubysAdventureKumarP1/Assets/Scripts/RubyController.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    int currentHealth;
+
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
 
-    //Start is calleed beeforee the first frame update
-    void start()
+    //Start is called before tthe first frame update 
+    void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        currentHealth = 1;
     }
 
-    //Update is called once per frame
-    void update()
+    //Update is called once per frame 
+    void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
@@ -23,10 +31,16 @@ public class RubyController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector2 position = rigidbody2d.position;
+        Vector2 position = transform.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+
+        public void ChangeHealth(int amount)
+        {
+            currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+            Debug.Log(currentHealth + "/" + maxHealth);
+        }
     }
 }
