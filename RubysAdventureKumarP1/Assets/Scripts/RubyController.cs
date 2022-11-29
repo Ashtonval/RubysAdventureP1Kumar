6189d092a75136109401a0429585af41702941ba
Ashtonval <tennisjedi20xx@gmail.com>
2022-11-29 09:52:38 -0800

    Fixed scripts and made health a prefab
    Scripts for Ruby and the Health collectible were enhanced, damageable object was added, and the health collectible was added as a prefab.

--- RubysAdventureKumarP1/Assets/Scripts/RubyController.cs
+++ RubysAdventureKumarP1/Assets/Scripts/RubyController.cs
@@ -7,22 +7,22 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-    int currentHealth;
 
+    public int health { get { return currentHealth; } }
+    int currentHealth;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
 
-    //Start is called before tthe first frame update 
+    // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        currentHealth = 1;
     }
 
-    //Update is called once per frame 
+    // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
@@ -31,16 +31,16 @@ public class RubyController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    }
 
-        public void ChangeHealth(int amount)
-        {
-            currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-            Debug.Log(currentHealth + "/" + maxHealth);
-        }
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
